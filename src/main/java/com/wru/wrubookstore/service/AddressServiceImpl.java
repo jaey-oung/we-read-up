2fb899225361225625f02f2a793a25d4b33e610a
Seungwon55 <blackstar55@naver.com>
2025-03-18 15:29:16 +0900

    AddressService 생성

---
+++ src/main/java/com/wru/wrubookstore/service/AddressServiceImpl.java
@@ -0,0 +1,22 @@
+package com.wru.wrubookstore.service;
+
+import com.wru.wrubookstore.dto.AddressDto;
+import com.wru.wrubookstore.repository.AddressRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    private final AddressRepository addressRepository;
+
+    public AddressServiceImpl(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+    @Override
+    public List<AddressDto> selectList(Integer memberId) throws Exception {
+        return addressRepository.selectList(memberId);
+    }
+}
