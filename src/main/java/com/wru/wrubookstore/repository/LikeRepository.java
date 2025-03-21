6553a728f148321beebb99fe41df44617a238807
Choi-hoe-jin <gkwksk3@gmail.com>
2025-03-21 11:45:22 +0900

    상품 추가 오류 수정 중

--- src/main/java/com/wru/wrubookstore/repository/LikeRepository.java
+++ src/main/java/com/wru/wrubookstore/repository/LikeRepository.java
@@ -1,14 +1,16 @@
 package com.wru.wrubookstore.repository;
 
+import com.wru.wrubookstore.dto.LikeDto;
+
 import java.util.Map;
 
 public interface LikeRepository {
     // 해당 책의 좋아요 수 조회
     Integer likeCount(Integer bookId) throws Exception;
     // 현재 유저가 해당 책을 좋아요 눌렀는지 확인
-    Integer selectLikeMember(Map map) throws Exception;
+    Integer selectLikeMember(LikeDto likeDto) throws Exception;
     // 해당 책을 좋아요에 추가
-    void insertLike(Map map) throws Exception;
+    void insertLike(LikeDto likeDto) throws Exception;
     // 해당 책에 누른 좋아요 삭제
-    void deleteLike(Map map) throws Exception;
+    void deleteLike(LikeDto likeDto) throws Exception;
 }
--- src/test/java/com/wru/wrubookstore/repository/BookRepositoryTest.java
+++ src/test/java/com/wru/wrubookstore/repository/BookRepositoryTest.java
@@ -3,6 +3,7 @@ package com.wru.wrubookstore.repository;
 
 import com.wru.wrubookstore.domain.PageHandler;
 import com.wru.wrubookstore.dto.BookDto;
+import com.wru.wrubookstore.service.BookService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,14 +22,29 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class BookRepositoryTest {
 
     @Autowired
-    BookRepository book;
+    BookService book;
 
     @Test
     public void select() throws Exception {
-        for (int i = 1; i < 250; i++) {
-            BookDto bookDto = new BookDto("pub_1","cs_1", ""+i, "김", 1000, new BigDecimal(0.01),170,1200, new Date(), new Date(), "목차", "내용", 1111111111, 100, "133*200", 420, 300,"https://image.aladin.co.kr/product/32875/63/cover500/k562936112_2.jpg");
+            BookDto bookDto = new BookDto("pub_1",
+                    "cs_1",
+                    "텐서플로",
+                    "김",
+                    1000,
+                    new BigDecimal(0.01),
+                    170,
+                    1200,
+                    new Date(),
+                    new Date(),
+                    "1장 컴퓨터는 데이터에서 배운다",
+                    "코드 실행만으로는 머신 러닝과 딥러닝을 충분히 이해할 수 없다. 머신 러닝과 딥러닝을 제대로 이해하고 싶다면 코드 외에도 관련 이론과 알고리즘의 뒤편에 있는 수학 개념을 알아야 한다. 이 책은 이해를 돕는 개념 설명, 머신 러닝과 딥러닝 핵심 알고리즘의 작동 방식과 사용 방법, 그 밑바탕이 되는 수학, 실용적인 예제, 빠지기 쉬운 함정을 피하는 방법까지 이론과 코드를 균형 있게 설명한다.",
+                    1111111111,
+                    100,
+                    "133*200",
+                    420,
+                    300,
+                    "https://image.aladin.co.kr/product/32875/63/cover500/k562936112_2.jpg");
             book.insert(bookDto);
-        }
     }
 
     @Test
