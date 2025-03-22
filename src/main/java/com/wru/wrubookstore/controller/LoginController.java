b647a149185b656560c578781e62166ea16282eb
jaey-oung <jaeywodud7@gmail.com>
2025-03-23 01:59:05 +0900

    타임리프에 로그인 기능 적용 (쿠키/세션 적용)

--- src/main/java/com/wru/wrubookstore/controller/LoginController.java
+++ src/main/java/com/wru/wrubookstore/controller/LoginController.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,23 +25,21 @@ public class LoginController {
         this.userService = userService;
     }
 
+    // 쿠키에 저장된 이메일을 모델에 추가
     @GetMapping("/login")
-    public String loginForm() {
+    public String loginForm(@CookieValue(name = "email", required = false) String rememberedEmail, Model model) {
+        model.addAttribute("rememberedEmail", rememberedEmail);
         return "login/login";
     }
 
     @PostMapping("/login")
     public String login(String email, String password, String redirectUrl, boolean rememberId, RedirectAttributes rattr,
-                        HttpServletRequest request, HttpServletResponse response) throws Exception {
+                        HttpServletRequest request, HttpServletResponse response) {
 
         boolean isFieldNullOrEmpty = email == null || email.trim().isEmpty() || password == null || password.trim().isEmpty();
         UserDto userDto = userService.login(email, password);
 
-        System.out.println("email = " + email);
-        System.out.println("password = " + password);
-        System.out.println("isFieldNullOrEmpty = " + isFieldNullOrEmpty);
-        System.out.println("userDto = " + userDto);
-
+        // 로그인 실패 시 이메일, 비밀번호, 메시지를 리다이렉트로 전달
         if(isFieldNullOrEmpty || userDto == null) {
             rattr.addFlashAttribute("email", email);
             rattr.addFlashAttribute("password", password);
@@ -47,11 +47,10 @@ public class LoginController {
             return "redirect:/login/login?redirectUrl="+redirectUrl;
         }
 
-        // 세션에 연결
+        // 세션에 사용자 정보 저장
         HttpSession session = request.getSession();
-
-        // userId 와 isMember 정보 저장
         session.setAttribute("userId", userDto.getUserId());
+        session.setAttribute("name", userDto.getName());
         session.setAttribute("isMember", userDto.getIsMember());
 
         // 쿠키 저장
@@ -62,9 +61,9 @@ public class LoginController {
         cookie.setMaxAge(!rememberId ? 0 : 60 * 60 * 24 * 30);
         response.addCookie(cookie);
 
+        // 리다이렉트 URL 설정
         redirectUrl = (redirectUrl == null || redirectUrl.isEmpty()) ? "/" : redirectUrl;
 
-        System.out.println("userDto = " + userDto);
         return "redirect:"+redirectUrl;
     }
 
