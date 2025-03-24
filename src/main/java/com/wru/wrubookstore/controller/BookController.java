6bdfdae36b28aeaae8e7b2d0c369c9e76a28ca2b
18hailey <hailey9459@gmail.com>
2025-03-24 20:17:27 +0900

    BookController에 카테고리 클릭 시 도서 조회 로직 설계

--- src/main/java/com/wru/wrubookstore/controller/BookController.java
+++ src/main/java/com/wru/wrubookstore/controller/BookController.java
@@ -4,17 +4,20 @@ import com.wru.wrubookstore.domain.PageHandler;
 import com.wru.wrubookstore.domain.MainSearchCondition;
 import com.wru.wrubookstore.domain.SearchCondition;
 import com.wru.wrubookstore.dto.BookDto;
+import com.wru.wrubookstore.dto.CategoryDto;
 import com.wru.wrubookstore.dto.ReviewDto;
 import com.wru.wrubookstore.dto.response.review.ReviewListResponse;
 import com.wru.wrubookstore.service.BookService;
 import com.wru.wrubookstore.service.LikeService;
 import com.wru.wrubookstore.service.ReviewService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,43 +35,30 @@ public class BookController {
     }
 
     @GetMapping("/bookList")
-    public String bookList(Integer page, String category, Integer pageSize,Model m) {
-        // 홈페이지에서 카테고리를 눌러서 들어온다.
-        // 카테고리 id가 넘어오고
-        // 카테고리 id로 해당 카테고리의 bookList를 보여준다.
+    public String bookList(MainSearchCondition sc, Model model, HttpServletRequest request,
+                           HttpSession session) {
 
-        // 1. 페이징
-        // page가 null 이면 기본값추가
-        if(page==null)
-            page=1;
-        // viewBookCnt가 null 이면 기본값추가
-        if(pageSize==null || pageSize==10)
-            pageSize=8;
-        // category가 null 이면 기본값추가
-        if(category==null)
-            category="cs_2";
+        int userId = (int) session.getAttribute("userId");
 
         try{
-            // 카테고리 id에 있는 책의 수를 가져온다.
-            int bookCnt = bookService.sCategoryCnt(category);
-            // 페이지 만듬
-            PageHandler pageHandler = new PageHandler(bookCnt, page, pageSize);
+            int count = bookService.selectByCategoryCnt(sc.getCategory());
+            List<CategoryDto> list = new ArrayList<>();
+            if (count == 0) {
+                CategoryDto categoryInfo = bookService.selectCategoryInfo(sc.getCategory());
+                model.addAttribute("category", categoryInfo);
+            } else if (count > 0) {
+                list = bookService.selectByCategory(sc);
+            } else {
+                throw new Exception("잘못된 도서 개수입니다.");
+            }
 
-            // 2. 책 정보 날려주기
-            // Map에 category, offset, limit를 put
-            Map map = new HashMap();
-            // PageHandler로 넘겨주기
-            map.put("category", category);
-            // 0-8-16-24
-            map.put("offset", (page-1)*pageSize);
-            // pageSize 만큼 뽑기
-            map.put("limit", pageSize);
-            List<BookDto> list = bookService.selectRegList(map);
+            PageHandler pageHandler = new PageHandler(count, sc.getPage(), sc.getPageSize());
 
-            // 3. Model 에 담아서 view 로 넘기기
-            m.addAttribute("ph", pageHandler);
-            m.addAttribute("list", list);
-            m.addAttribute("category", category);
+            model.addAttribute("sc", sc);
+            model.addAttribute("userId", userId);
+            model.addAttribute("list", list);
+            model.addAttribute("ph", pageHandler);
+            model.addAttribute("uri", request.getRequestURI());
         } catch(Exception e) {
             e.printStackTrace();
         }
