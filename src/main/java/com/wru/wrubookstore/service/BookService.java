90ff4930fa718aa911a26107f6f28e797af7d77b
18hailey <hailey9459@gmail.com>
2025-03-24 20:14:51 +0900

    BookService에 카테고리 도서 조회 메서드 정의

--- src/main/java/com/wru/wrubookstore/service/BookService.java
+++ src/main/java/com/wru/wrubookstore/service/BookService.java
@@ -2,19 +2,23 @@ package com.wru.wrubookstore.service;
 
 import com.wru.wrubookstore.domain.MainSearchCondition;
 import com.wru.wrubookstore.dto.BookDto;
+import com.wru.wrubookstore.dto.CategoryDto;
 
 import java.util.List;
 import java.util.Map;
 
 public interface BookService {
-    // 책 번호로 한개 조회
+    // 카테고리 정보 조회
+    CategoryDto selectCategoryInfo(String category) throws Exception;
+
+    // 카테고리에 속한 책들의 수 조회
+    int selectByCategoryCnt(String category) throws Exception;
+
+    // 카테고리에 속한 책들의 정보 조회
+    List<CategoryDto> selectByCategory(MainSearchCondition sc) throws Exception;
+
+    // 책 번호로 한 개 조회
     BookDto select(Integer bookId) throws Exception;
-
-    // LIMIT로 N개 조회 category(카테고리 소), offset, limit
-    List<BookDto> selectRegList(Map map) throws Exception;
-
-    // 카테고리에 있는 책 수 조회
-    int sCategoryCnt(String category) throws Exception;
 
     // 테스트용 insert
     void insert(BookDto book) throws Exception;
