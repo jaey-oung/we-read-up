acdc95ab365a43f759cec8082075cf1ddb356e18
YOOCHANGHO <bokco99@gmail.com>
2025-03-24 16:31:12 +0900

    notice_board 25_03_24 dao -> repository 변경

--- src/main/java/com/wru/wrubookstore/service/CommentService.java
+++ src/main/java/com/wru/wrubookstore/service/CommentService.java
@@ -1,9 +1,7 @@
 package com.wru.wrubookstore.service;
 
-import com.wru.wrubookstore.dao.CommentDao;
-import com.wru.wrubookstore.dao.NoticeDao;
+import com.wru.wrubookstore.repository.CommentRepositoryImpl;
 import com.wru.wrubookstore.dto.CommentDto;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,14 +10,14 @@ import java.util.List;
 @Service
 public class CommentService {
 
-    private final CommentDao commentDao;
+    private final CommentRepositoryImpl commentRepositoryImpl;
 
-    public CommentService(CommentDao commentDao) {
-        this.commentDao = commentDao;
+    public CommentService(CommentRepositoryImpl commentRepositoryImpl) {
+        this.commentRepositoryImpl = commentRepositoryImpl;
     }
 
     public int getCount(Integer noticeId) throws Exception {
-        return commentDao.count(noticeId);
+        return commentRepositoryImpl.count(noticeId);
     }
 
     @Transactional(rollbackFor = Exception.class)
@@ -27,7 +25,7 @@ public class CommentService {
 //        int rowCnt = noticeDao.updateCommentCnt(noticeId, -1);
 //        System.out.println("updateCommentCnt - rowCnt = " + rowCnt);
 ////        throw new Exception("test");
-        int rowCnt = commentDao.delete(commentId, memberId);
+        int rowCnt = commentRepositoryImpl.delete(commentId, memberId);
         System.out.println("rowCnt = " + rowCnt);
         return rowCnt;
     }
@@ -36,19 +34,19 @@ public class CommentService {
     public int write(CommentDto commentDto) throws Exception {
 //        noticeDao.updateCommentCnt(commentDto.getBno(), 1);
 //                throw new Exception("test");
-        return commentDao.insert(commentDto);
+        return commentRepositoryImpl.insert(commentDto);
     }
 
     public List<CommentDto> getList(Integer noticeId) throws Exception {
 //        throw new Exception("test");
-        return commentDao.selectAll(noticeId);
+        return commentRepositoryImpl.selectAll(noticeId);
     }
 
     public CommentDto read(Integer commentId) throws Exception {
-        return commentDao.select(commentId);
+        return commentRepositoryImpl.select(commentId);
     }
 
     public int modify(CommentDto commentDto) throws Exception {
-        return commentDao.update(commentDto);
+        return commentRepositoryImpl.update(commentDto);
     }
 }
