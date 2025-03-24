7bd4bfe180f7723f51e1100631a92fe98368681c
YOOCHANGHO <bokco99@gmail.com>
2025-03-24 21:18:55 +0900

    faq 25_03_24 조회기능 작성완료

--- src/main/java/com/wru/wrubookstore/service/FaqService.java
+++ src/main/java/com/wru/wrubookstore/service/FaqService.java
@@ -1,41 +1,17 @@
 package com.wru.wrubookstore.service;
 
 import com.wru.wrubookstore.dto.FaqDto;
-import com.wru.wrubookstore.repository.FaqRepository;
-import com.wru.wrubookstore.repository.FaqRepositoryImpl;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class FaqService {
+public interface FaqService {
+    List<FaqDto> getList() throws Exception;
 
-    private final FaqRepository faqRepository;
+    FaqDto read(Integer faqId) throws Exception;
 
-    public FaqService(FaqRepositoryImpl faqRepository) {
-        this.faqRepository = faqRepository;
-    }
+    Integer remove(Integer faqId, String employeeId) throws Exception;
 
-    public List<FaqDto> getList() throws Exception{
-        return faqRepository.selectAll();
-    }
+    Integer write(FaqDto faqDto) throws Exception;
 
-    public FaqDto read(Integer faqId) throws Exception{
-        FaqDto faqDto = faqRepository.select(faqId);
-
-        return faqDto;
-    }
-
-    public Integer remove(Integer faqId, String employeeId) throws Exception{
-        return faqRepository.delete(faqId, employeeId);
-    }
-
-    public Integer write(FaqDto faqDto) throws Exception{
-        return faqRepository.insert(faqDto);
-    }
-
-
-
-
-
+    Integer modify(FaqDto faqDto) throws Exception;
 }
