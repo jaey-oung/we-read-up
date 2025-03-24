239f6be05e988c16e8cac7c486f9c048b61846b0
YOOCHANGHO <bokco99@gmail.com>
2025-03-24 17:55:14 +0900

    faq 25_03_24 mapper, dto, repository 작성

---
+++ src/main/java/com/wru/wrubookstore/service/FaqService.java
@@ -0,0 +1,32 @@
+package com.wru.wrubookstore.service;
+
+import com.wru.wrubookstore.dto.FaqDto;
+import com.wru.wrubookstore.repository.FaqRepository;
+import com.wru.wrubookstore.repository.FaqRepositoryImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FaqService {
+
+    private final FaqRepository faqRepository;
+
+    public FaqService(FaqRepositoryImpl faqRepository) {
+        this.faqRepository = faqRepository;
+    }
+
+    public List<FaqDto> getList() throws Exception{
+        return faqRepository.selectAll();
+    }
+
+    public FaqDto read(Integer faqId) throws Exception{
+        FaqDto faqDto = faqRepository.select(faqId);
+
+        return faqDto;
+    }
+
+
+
+
+}
