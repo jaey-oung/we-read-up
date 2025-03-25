b15ad50fa31d1ed7a2caab623bb832b6793e5649
18hailey <hailey9459@gmail.com>
2025-03-25 11:26:25 +0900

    BookRepositoryImpl 빠진 import 문 추가

--- src/main/java/com/wru/wrubookstore/repository/BookRepositoryImpl.java
+++ src/main/java/com/wru/wrubookstore/repository/BookRepositoryImpl.java
@@ -8,6 +8,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
