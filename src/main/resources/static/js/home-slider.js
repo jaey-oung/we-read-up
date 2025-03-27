89a1e815477b5e7ba50564235c239b6701ad7a4f
18hailey <hailey9459@gmail.com>
2025-03-27 15:29:41 +0900

    슬라이드 이미지 배열 생성

---
+++ src/main/resources/static/js/home-slider.js
@@ -0,0 +1,39 @@
+ (() => {
+    const images_list = [
+        {
+            "url": "/img/slides/new-java.png",
+            "name": "신간도서_자바의_정석_4판",
+            "link": "/search?option=all&keyword=자바의+정석+4판" /* 책 추가 후 bookDetail로 연결 */
+        },
+        {
+            "url": "/img/slides/rec-health.png",
+            "name": "추천_건강의_날_건강도서",
+            "link": "/bookList?category=cm_1" /* 국내 건강 도서 기준 */
+        },
+        {
+            "url": "/img/slides/event-join.png",
+            "name": "회원가입_이벤트",
+            "link": "/register/form"
+        },
+        {
+            "url": "/img/slides/new-golf.png",
+            "name": "신간도서_골프",
+            "link": "/search?option=all&keyword=골프" /* 책 추가 후 bookDetail로 연결 */
+        },
+        {
+            "url": "/img/slides/event-spring.png",
+            "name": "봄_이벤트_쿠폰",
+            "link": "/search?option=all&keyword=" /* 국내 도서 전체 리스트 출력 */
+        },
+        {
+            "url": "/img/slides/rec-travel.png",
+            "name": "추천_여행도서",
+            "link": "/bookList?category=cm_2" /* 국내 여행 도서 기준 */
+        },
+        {
+            "url": "/img/slides/benefit-member.png",
+            "name": "회원_혜택",
+            "link": "/login/login"
+        }
+    ];
+})();
